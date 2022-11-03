7a7ec5646cfb3b2a67afef4fdd80c610cf442761
htmlgod <ivannevtrilov212@gmail.com>
2022-11-03 05:49:39 +0300

    tcp

--- remote_management.cpp
+++ remote_management.cpp
@@ -1,152 +1,110 @@
 #include "remote_management.h"
 #include "ui_remote_management.h"
 
+qint16 cur_client;
+QSet<QTcpSocket*> CLIENTS;
+QMap<qint16, QPushButton*> CLIENT_TO_SLOT;
+QMap<QPushButton*, qint16> SLOT_TO_CLIENT;
+QQueue<QPushButton*> CLIENT_SLOTS;
+
+
 remote_management::remote_management(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-    management_socket = new QUdpSocket(this);
-    if (!management_socket->bind(QHostAddress::AnyIPv4, 1225)) {
+    mgm_socket = new mgm_server(this, ui->preview_scr);
+    if (!mgm_socket->listen(QHostAddress::AnyIPv4, 1225)) {
         qDebug() << "ERROR OPEN SOCK";
         return;
     }
-    connect(management_socket, &QUdpSocket::readyRead, this, &remote_management::recieve_management_data);
+
     ui->cl_slot0->setFocus();
-    cl_slots.append(ui->cl_slot0);
-    cl_slots.append(ui->cl_slot1);
-    cl_slots.append(ui->cl_slot2);
+    CLIENT_SLOTS.append(ui->cl_slot0);
+    CLIENT_SLOTS.append(ui->cl_slot1);
+    CLIENT_SLOTS.append(ui->cl_slot2);
 }
-void remote_management::process_preview(const QByteArray& data, int client) {
+// WIP UDP TO TCP XLATE
+remote_management::mgm_server::mgm_server(QObject *parent, QLabel* preview_scr) : QTcpServer(parent), preview_screen(preview_scr){}
+
+void remote_management::mgm_server::readyRead()
+{
+    QTcpSocket *client = (QTcpSocket*)sender();
+    auto cl_port = client->peerPort();
+    QByteArray data = client->readAll();
     QPixmap pxm;
     QByteArray uncompressed = qUncompress(data);
     pxm.loadFromData(uncompressed, "JPG");
-
-    if (cur_client == client) ui->preview_scr->setPixmap(pxm);
+//if (cur_client == cl_port)
+    preview_screen->setPixmap(pxm);
 
     // fix later
-    auto pxm_scaled = pxm.scaled(ui->cl_slot0->size().height() - 5, ui->cl_slot0->size().width() - 5);
-    QIcon qi(pxm_scaled);
-    qDebug() << "icon size" << qi.actualSize(ui->cl_slot0->size());
-    auto tmp = cl_con.value(client);
+    //auto pxm_scaled = pxm.scaled(ui->cl_slot0->size().height() - 5, ui->cl_slot0->size().width() - 5);
+    //QIcon qi(pxm_scaled);
+    QIcon qi(pxm);
+    //qDebug() << "icon size" << qi.actualSize(ui->cl_slot0->size());
+    auto tmp = CLIENT_TO_SLOT.value(cl_port);
     tmp->setIcon(qi);
     tmp->setText("");
-    tmp->setIconSize(pxm_scaled.rect().size());
+    //tmp->setIconSize(pxm_scaled.rect().size());
+    tmp->setIconSize(pxm.rect().size());
     tmp->update();
 }
-void remote_management::process_client(const QNetworkDatagram& dg) {
-    QByteArray data = dg.data();
-    QDataStream ds(&data, QIODevice::ReadOnly);
-    protocol_msg_data msg;
-    ds >> msg;
-    if (msg.msg == "SCR") {
-        qDebug() << "Incoming preview from client " << dg.senderPort();
-        process_preview(msg.data, dg.senderPort());
-    }
-    else if (msg.msg == "DSC") {
-        qDebug() << "Pending disconnection from client " << dg.senderPort();
-        process_client_disconnect(dg);
-        qDebug() << "Client " << dg.senderPort() << " disconnected";
-    }
-    else if (msg.msg == "CON") {
-        qDebug() << "Pending connection from client " << dg.senderPort();
-        process_new_client(dg);
-        qDebug() << "Client " << dg.senderPort() << " connected";
-    }
-    else {
-        protocol_msg_data ans = {
-            "ERR",
-            0
-        };
-        QByteArray data;
-        QDataStream answer(&data, QIODevice::WriteOnly);
-        answer << ans;
-        management_socket->writeDatagram(data, dg.senderAddress(), dg.senderPort());
-        qDebug() << "Client " << dg.senderPort() << " failed to connect: ";
-    }
 
+void remote_management::mgm_server::disconnected()
+{
+    QTcpSocket *client = (QTcpSocket*)sender();
+    auto cl_port = client->peerPort();
+
+    CLIENTS.remove(client);
+    auto tmp = CLIENT_TO_SLOT.take(cl_port);
+    tmp->setIcon(QIcon());
+    tmp->setText("ОЖИДАНИЕ КЛИЕНТА...");
+    tmp->update();
+    CLIENT_TO_SLOT.remove(cl_port);
+    SLOT_TO_CLIENT.remove(tmp);
+    CLIENT_SLOTS.append(tmp);
+    qDebug() << "Client " << cl_port << " disconnected";
 }
-void remote_management::process_new_client(const QNetworkDatagram& dg) {
-    protocol_msg_data ans = {
-        "OK",
-        0
-    };
-    if (clients.contains(dg.senderPort())) {
-        ans.msg = "ERR";
-    }
-    else {
-        clients.insert(dg.senderPort());
-        auto tmp = cl_slots.head();
-        cl_slots.pop_front();
-        cl_con.insert(dg.senderPort(), tmp);
-        con_cl.insert(tmp, dg.senderPort());
-    }
-    QByteArray ans_data;
-    QDataStream answer(&ans_data, QIODevice::WriteOnly);
-    answer << ans;
 
-    management_socket->writeDatagram(ans_data, dg.senderAddress(), dg.senderPort());
-    qDebug() << "Connection request answer sended to " << dg.senderPort();
+void remote_management::mgm_server::incomingConnection(qintptr socketfd)
+{
+    QTcpSocket *client = new QTcpSocket(this);
+    client->setSocketDescriptor(socketfd);
+    CLIENTS.insert(client);
+
+    qDebug() << "New client from:" << client->peerPort();
+
+    connect(client, SIGNAL(readyRead()), this, SLOT(readyRead()));
+    connect(client, SIGNAL(disconnected()), this, SLOT(disconnected()));
+    auto cl_port = client->peerPort();
+     cur_client = cl_port;
+    auto tmp = CLIENT_SLOTS.head();
+    CLIENT_SLOTS.pop_front();
+    CLIENT_TO_SLOT.insert(cl_port, tmp);
+    SLOT_TO_CLIENT.insert(tmp, cl_port);
+
     server_settings_data server_settings {
         y_res,x_res,img_format,compression, preview_upd, xmit_upd
     };
     QByteArray settings_data;
-    QDataStream settings(&settings_data, QIODevice::WriteOnly);
+    QDataStream settings(&settings_data, QIODevice::ReadWrite);
+    settings.setVersion(QDataStream::Qt_5_10);
     settings << server_settings;
-    management_socket->writeDatagram(settings_data, dg.senderAddress(), dg.senderPort());
-    qDebug() << "Settings sended to " << dg.senderPort();
-}
-void remote_management::process_client_disconnect(const QNetworkDatagram& dg) {
-    protocol_msg_data ans = {
-        "OK",
-        0
-    };
-    if (!clients.contains(dg.senderPort())) {
-        ans.msg = "ERR";
-    }
-    else {
-        clients.remove(dg.senderPort());
-        auto tmp = cl_con.take(dg.senderPort());
-        tmp->setIcon(QIcon());
-        tmp->setText("ОЖИДАНИЕ КЛИЕНТА...");
-        tmp->update();
-        cl_con.remove(dg.senderPort());
-        con_cl.remove(tmp);
-        cl_slots.append(tmp);
-        qDebug() << "Client " << dg.senderPort() << " disconnected";
-    }
-    QByteArray data;
-    QDataStream answer(&data, QIODevice::WriteOnly);
-    answer << ans;
-    management_socket->writeDatagram(data, dg.senderAddress(), dg.senderPort());
-}
-void remote_management::recieve_management_data() {
-    // distinguish from which client came preview
-    while (management_socket->hasPendingDatagrams()) {
-        auto dg_size = management_socket->pendingDatagramSize();
-        QNetworkDatagram dg = management_socket->receiveDatagram();
-
-        std::stringstream ss;
-        ss << "From " << dg.senderAddress().toString().toStdString();
-        ss << ":" << dg.senderPort() << " recieved " << dg_size;
-       // qDebug() << QString::fromStdString(ss.str());
-
-        process_client(dg);
-    }
+    client->write(settings_data);
+    qDebug() << "Settings sended to " << cl_port;
 }
 
 remote_management::~remote_management()
 {
     delete ui;
+    delete mgm_socket;
 }
 
 void remote_management::on_cl_slot0_clicked()
 {
     ui->stackedWidget->setCurrentIndex(0);
-    cur_client = con_cl.value(ui->cl_slot0);
-//    if (sender() == ui->cl_slot0) {
-
-//    }
+    cur_client = SLOT_TO_CLIENT.value(ui->cl_slot0);
 }
 
 void remote_management::on_next_page_2_clicked()
@@ -157,11 +115,13 @@ void remote_management::on_next_page_2_clicked()
 void remote_management::on_cl_slot1_clicked()
 {
     ui->stackedWidget->setCurrentIndex(0);
-    cur_client = con_cl.value(ui->cl_slot1);
+    cur_client = SLOT_TO_CLIENT.value(ui->cl_slot1);
 }
 
 void remote_management::on_cl_slot2_clicked()
 {
     ui->stackedWidget->setCurrentIndex(0);
-    cur_client = con_cl.value(ui->cl_slot2);
+    cur_client = SLOT_TO_CLIENT.value(ui->cl_slot2);
 }
+
+
--- remote_management.h
+++ remote_management.h
@@ -7,7 +7,10 @@
 #include <QtNetwork>
 #include <QtWidgets>
 #include <sstream>
-//#include <zlib.h>
+
+#include <QTcpServer>
+#include <QTcpSocket>
+
 #include <QPixmap>
 #include "remote_control.h"
 
@@ -15,12 +18,60 @@ QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
 QT_END_NAMESPACE
 
+extern qint16 cur_client;
+extern QSet<QTcpSocket*> CLIENTS;
+extern QMap<qint16, QPushButton*> CLIENT_TO_SLOT;
+extern QMap<QPushButton*, qint16> SLOT_TO_CLIENT;
+extern QQueue<QPushButton*> CLIENT_SLOTS;
+
+
+struct protocol_msg_data {
+    QString msg;
+    QByteArray data;
+    // crypto
+    bool operator==(const protocol_msg_data& o){
+        return o.msg == this->msg && o.data == this->data;
+
+    }
+    friend QDataStream &operator<<(QDataStream& out, const protocol_msg_data& rhs){
+        out << rhs.data << rhs.msg;
+        return out;
+    }
+    friend QDataStream &operator>>(QDataStream& in, protocol_msg_data& rhs){
+        in  >> rhs.data >> rhs.msg;
+        return in;
+    }
+};
+const QString y_res = "720";
+const QString x_res = "1280";
+const QString img_format = "JPG";
+const QString compression = "9";
+const QString preview_upd = "3"; // secs
+const QString xmit_upd = "0.05"; // secs
+struct server_settings_data {
+
+    QString y_res;
+    QString x_res;
+    QString img_format;
+    QString compression;
+    QString preview_upd;
+    QString xmit_upd;
+    //bool operator==(const server_settings_data& o) const = default;
+    friend QDataStream &operator<<(QDataStream& out, const server_settings_data& server_settings){
+        out << server_settings.y_res <<  server_settings.x_res <<  server_settings.img_format <<  server_settings.compression <<  server_settings.preview_upd <<  server_settings.xmit_upd;
+        return out;
+    }
+    friend QDataStream &operator>>(QDataStream& in, server_settings_data& server_settings){
+        in >> server_settings.y_res >>  server_settings.x_res >>  server_settings.img_format >>  server_settings.compression >>  server_settings.preview_upd >>  server_settings.xmit_upd;
+        return in;
+    }
+};
+
 class remote_management : public QMainWindow
 {
     Q_OBJECT
 
 private slots:
-    void recieve_management_data();
 
     void on_cl_slot0_clicked();
 
@@ -34,63 +85,32 @@ public:
     remote_management(QWidget *parent = nullptr);
     ~remote_management();
 
-    void process_preview(const QByteArray& data, int client);
-    void process_client(const QNetworkDatagram& dg);
-    void process_new_client(const QNetworkDatagram& dg);
-    void process_client_disconnect(const QNetworkDatagram& dg);
-    void process_key_exchange(const QNetworkDatagram& dg);
+//    void init_new_client(qintptr cl);
+//    void send_settings(qintptr cl);
+//    void exchange_keys(qintptr cl);
 
 private:
+    class mgm_server;
+
+    mgm_server* mgm_socket;
     Ui::MainWindow *ui;
-    QUdpSocket* management_socket;
-
-    struct protocol_msg_data {
-        QString msg;
-        QByteArray data;
-        // crypto
-        bool operator==(const protocol_msg_data& o){
-            return o.msg == this->msg && o.data == this->data;
-
-        }
-        friend QDataStream &operator<<(QDataStream& out, const protocol_msg_data& rhs){
-            out << rhs.data << rhs.msg;
-            return out;
-        }
-        friend QDataStream &operator>>(QDataStream& in, protocol_msg_data& rhs){
-            in  >> rhs.data >> rhs.msg;
-            return in;
-        }
-    };
-
-    struct server_settings_data {
-        QString y_res;
-        QString x_res;
-        QString img_format;
-        QString compression;
-        QString preview_upd;
-        QString xmit_upd;
-        //bool operator==(const server_settings_data& o) const = default;
-        friend QDataStream &operator<<(QDataStream& out, const server_settings_data& server_settings){
-            out << server_settings.y_res <<  server_settings.x_res <<  server_settings.img_format <<  server_settings.compression <<  server_settings.preview_upd <<  server_settings.xmit_upd;
-            return out;
-        }
-        friend QDataStream &operator>>(QDataStream& in, server_settings_data& server_settings){
-            in >> server_settings.y_res >>  server_settings.x_res >>  server_settings.img_format >>  server_settings.compression >>  server_settings.preview_upd >>  server_settings.xmit_upd;
-            return in;
-        }
-    };
-    QSet<int> clients;
-    QMap<int, QPushButton*> cl_con;
-    QMap<QPushButton*, int> con_cl;
-    QQueue<QPushButton*> cl_slots;
-
-    int cur_client;
-    //settings
-    const QString y_res = "720";
-    const QString x_res = "1280";
-    const QString img_format = "JPG";
-    const QString compression = "9";
-    const QString preview_upd = "3"; // secs
-    const QString xmit_upd = "0.05"; // secs
 };
+class remote_management::mgm_server : public QTcpServer
+{
+    Q_OBJECT
+
+    public:
+        mgm_server(QObject *parent=0, QLabel* preview_screen=0);
+
+    private slots:
+        void readyRead();
+        void disconnected();
+
+    protected:
+        void incomingConnection(qintptr socketfd);
+    private:
+        QLabel* preview_screen;
+        QDataStream in;
+};
+
 #endif // MAINWINDOW_H
